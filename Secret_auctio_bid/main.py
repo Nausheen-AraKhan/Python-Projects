10f524909b8e6ae19ce9ad01c6510a01875f513f
Nausheen Ara Khan <nausheenara1909khan@gmail.com>
2026-06-13 21:42:15 +0530

    Implement auction bidding functionality

---
+++ Secret_auctio_bid/main.py
@@ -0,0 +1,16 @@
+import art
+print(art.logo)
+yes_no="YES"
+auction_bit={}
+while yes_no == "YES":
+    name=input("What is your name? ")
+    bid=int(input("What is your bid? $"))
+    auction_bit[name]=bid
+    yes_no=input("Are there any more bidders? Type 'YES' or 'NO': ").upper()
+    if yes_no == "NO":
+        break
+    else:
+        print("\n"*50)
+
+winner=max(auction_bit, key=auction_bit.get)
+print(f"The winner is {winner} with a bid of ${auction_bit[winner]}")
